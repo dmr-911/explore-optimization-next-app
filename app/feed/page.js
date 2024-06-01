42d6bbe92d53d8b29fc4614c0b6e363338d311cb
dmr-911 <dewan.mizanur911@gmail.com>
2024-06-01 20:42:38 +0600

    feat: metadata and dynamic metadata explored

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,19 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// export const metadata = {
+//   title: "something",
+//   description: "something",
+// };
+
+export const generateMetadata = async ({ params, searchParams }) => {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `posts-${numberOfPosts}`,
+    description: "lulu",
+  };
+};
 
 export default async function FeedPage() {
   const posts = await getPosts();
